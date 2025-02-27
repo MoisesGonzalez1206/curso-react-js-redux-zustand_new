d7a7929492348d405da468a3188c80cb22decad2
MoisesGonzalez1206 <moises.gonzalezs@axity.com>
2025-02-27 14:30:20 -0600

    second_configuration commit :tada:

---
+++ agenda/test/services/auth.services.test.tsx
@@ -0,0 +1,35 @@
+import { loginService } from '../../src/services/auth.service';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter'
+import {describe, it, expect, beforeEach, afterEach} from '@jest/globals'
+import { AuthResponse } from '../../src/types';
+
+describe('auth.service.tsx', () => { 
+    let mock: InstanceType<typeof MockAdapter>
+
+    beforeEach(() => {
+        mock = new MockAdapter(axios)
+    })
+
+    afterEach(() => {   
+        mock.restore()
+    })
+
+    it('should return status 200 on success login', async() => {
+        const username: string = 'Test'
+        const password: string = 'Test'
+        const mockResponse : AuthResponse = {status: 200, message: 'Login exitoso'}
+        mock.onPost('http://localhost:8080/login',{username, password}).reply(200, mockResponse)
+        const response = await loginService(username, password);
+        expect(response.status).toBe(200);
+    });
+
+    it('should return an error if the login failed', async() => {
+        const username: string = 'Test'
+       const password: string = 'Test'
+       const mockResponse : AuthResponse = {status: 401, message: 'Login fallido'}
+       mock.onPost('http://localhost:8080/login',{username, password}).reply(401, mockResponse)
+       await expect(loginService(username, password)).rejects.toThrowError("Algo fallo al iniciar sesion");
+   });
+    
+})
